031d2b2fb6ff4ac7e84e607c109760e908df47e5
Mykhailo Polishchuk <mykhailo.polishchuk@valthsolutions.com>
2025-06-02 16:09:03 +0300

    3

--- src/entry.py
+++ src/entry.py
@@ -1,34 +1,27 @@
 from workers import fetch, Response
 from js import Request, Headers
 
-async def on_fetch(request):
-    # Create a new Headers object, copying original headers
-    new_headers = Headers.new(request.headers)
-    # Set the new Host header
-    new_headers.set("My-header", "pluggedtable.com")
-    
-    # Create a new Request with the original request info and new headers
-    new_request = Request.new(request, {"headers": new_headers})
-    
-    return await fetch(new_request)
-
-
 # async def on_fetch(request):
-#     response = await fetch("https://example.com")
+#     # Create a new Headers object, copying original headers
+#     new_headers = Headers.new(request.headers)
+#     # Set the new Host header
+#     new_headers.set("My-header", "pluggedtable.com")
+    
+#     # Create a new Request with the original request info and new headers
+#     new_request = Request.new(request, {"headers": new_headers})
+    
+#     return await fetch(new_request)
 
-#     # Grab the response headers so they can be modified
-#     new_headers = response.headers
 
-#     # Add a custom header with a value
-#     new_headers["x-workers-hello"] = "Hello from Cloudflare Workers"
+async def on_fetch(request):
+    new_headers = Headers.new(request.headers)
+    new_headers.delete("host")
 
-#     # Delete headers
-#     if "x-header-to-delete" in new_headers:
-#         del new_headers["x-header-to-delete"]
-#     if "x-header2-to-delete" in new_headers:
-#         del new_headers["x-header2-to-delete"]
+    new_headers.set("My-header", "pluggedtable.com")
 
-#     # Adjust the value for an existing header
-#     new_headers["x-header-to-change"] = "NewValue"
+    new_request = Request.new("https://pluggedtable.com", {
+        "method": "GET",
+        "headers": new_headers,
+    })
 
-#     return Response(response.body, headers=new_headers)
+    return await fetch(new_request)
