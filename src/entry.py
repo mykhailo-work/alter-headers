2d93ff6c70b64fc5acbbb7e6bedf9c3d68206120
Mykhailo Polishchuk <mykhailo.polishchuk@valthsolutions.com>
2025-06-02 16:38:03 +0300

    version 2

--- src/entry.py
+++ src/entry.py
@@ -1,14 +1,21 @@
-from workers import fetch
+from workers import fetch, URL
 from js import Request, Headers
 from urllib.parse import urlparse, urlunparse
 
+# async def on_fetch(request, env):
+#     url = urlparse(request.url)
+#     modified_url = urlunparse(url._replace(netloc=env.API_HOST))
+
+#     new_headers = Headers.new(request.headers)
+#     new_headers.delete("host")
+
+#     new_request = Request.new(modified_url, {"headers": new_headers})
+
+#     return await fetch(new_request)
+
+
 async def on_fetch(request, env):
-    url = urlparse(request.url)
-    modified_url = urlunparse(url._replace(netloc=env.API_HOST))
+    url = URL.new(request.url)
+    url.hostname = env.API_HOST
 
-    new_headers = Headers.new(request.headers)
-    new_headers.delete("host")
-
-    new_request = Request.new(modified_url, {"headers": new_headers})
-
-    return await fetch(new_request)
+    return await fetch(url.toString(), request)
