1a1b8b30028977b3ff91d16144847479df7e1370
Mykhailo Polishchuk <mykhailo.polishchuk@valthsolutions.com>
2025-06-05 14:36:59 +0300

    test

--- src/entry.py
+++ src/entry.py
@@ -7,11 +7,8 @@ async def on_fetch(request, env):
     url.hostname = env.API_HOST
     
     headers = Headers.new(request.headers)
-    headers.set("x-manatal-custom-hostname", custom_hostname)
+    headers.set("x-forwarded-host", custom_hostname)
 
     new_request = Request.new(url.toString(), method=request.method, headers=headers, body=request.body)
-    print(new_request.body)
-    print(new_request.headers)
-    print(new_request.headers.get("x-manatal-custom-hostname"))
 
     return await fetch(new_request)
