be82f8ed26416572b52d0143d6d271dddfa5a8db
Mykhailo Polishchuk <mykhailo.polishchuk@valthsolutions.com>
2025-06-05 13:28:18 +0300

    test2

--- src/entry.py
+++ src/entry.py
@@ -4,17 +4,12 @@ async def on_fetch(request, env):
     url = URL.new(request.url)
     url.hostname = env.API_HOST
 
-    # Clone the original headers
     # headers = Headers.new(request.headers)
     # headers.set("Host", env.API_HOST)
 
-    # Recreate the request with the updated URL and headers
     new_request = Request.new(
-        url.toString(),
+        request,
         {
-            "method": request.method,
-            "headers": request.headers,
-            "body": await request.text() if request.method not in ["GET", "HEAD"] else None,
             "redirect": "manual"
         }
     )
