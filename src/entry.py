5d1f4c01ee06f177fb775b48351571e0fd7fb31d
Mykhailo Polishchuk <mykhailo.polishchuk@valthsolutions.com>
2025-06-05 14:00:45 +0300

    test4

--- src/entry.py
+++ src/entry.py
@@ -1,11 +1,14 @@
-from js import fetch, URL, Request
+from js import fetch, URL, Request, Headers
 
 async def on_fetch(request, env):
     url = URL.new(request.url)
+    
+    custom_hostname = url.hostname
     url.hostname = env.API_HOST
 
-    new_request = Request.new(
-        url.toString(), method=request.method, headers=request.headers, body=request.body
-    )
+    new_request = Request.new(url.toString(), method=request.method, headers=request.headers, body=request.body)
+    request.headers["x-manatal-custom-hostname"] = custom_hostname
+    print(request.body)
+    print(request.headers)
 
     return await fetch(new_request)
